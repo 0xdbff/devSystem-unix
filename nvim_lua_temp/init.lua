63252230ecf002147b47acc2392e16cfcaeb2682
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2022-02-03 23:42:48 +0000

    updated commits

--- nvim_lua_temp/init.lua
+++ nvim_lua_temp/init.lua
@@ -17,35 +17,48 @@ local use = require('packer').use
 require('packer').startup(function()
   -- Package manager
   use 'wbthomason/packer.nvim'
+  --
+  ------ Utils
   -- Git integration
   use 'tpope/vim-fugitive'
   -- Git integration :help compl-omni
   use 'tpope/vim-rhubarb'
   -- Make comments easier
   use 'numToStr/Comment.nvim'
-  -- Relative lines on current normal mode buf only
+  -- Relative lines on current buf(normal mode) only
   use 'jeffkreeftmeijer/vim-numbertoggle'
-
-  use 'ludovicchabant/vim-gutentags' -- Automatic tags management
-  -- UI to select things (files, grep results, open buffers...)
-  use {'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  -- Automatic tags management
+  use 'ludovicchabant/vim-gutentags'
+  --
+  ------ UI settings
+  -- select files, grep results, open buffers...
+  use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'} }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
-  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  -- Colorshemes
+  use 'mjlbach/onedark.nvim'
+  -- Status line
+  use 'nvim-lualine/lualine.nvim'
   -- Add indentation guides even on blank lines
   use 'lukas-reineke/indent-blankline.nvim'
   -- Add git related info in the signs columns and popups
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'} }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
   use 'nvim-treesitter/nvim-treesitter'
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
-
-  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  --
+  ------ LSP settings 
+  -- Collection of configurations for built-in LSP client
+  use 'neovim/nvim-lspconfig'
+  -- Autocomplete plugins
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lua'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  -- Snippets plugin
+  use 'L3MON4D3/LuaSnip'
 end) 
 
 --Set highlight on search
