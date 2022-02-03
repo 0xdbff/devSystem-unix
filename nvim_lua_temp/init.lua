69c4c21cf46931afba8e88c92481667f65674005
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2022-02-03 20:08:15 +0000

    updating dotfiles

--- nvim_lua_temp/init.lua
+++ nvim_lua_temp/init.lua
@@ -1,6 +1,7 @@
--- Install packer
+-- packer path
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
+-- Install packer
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
@@ -14,13 +15,20 @@ vim.cmd [[
 
 local use = require('packer').use
 require('packer').startup(function()
-  use 'wbthomason/packer.nvim' -- Package manager
-  use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+  -- Git integration
+  use 'tpope/vim-fugitive'
+  -- Git integration :help compl-omni
+  use 'tpope/vim-rhubarb'
+  -- Make comments easier
+  use 'numToStr/Comment.nvim'
+  -- Relative lines on current normal mode buf only
+  use 'jeffkreeftmeijer/vim-numbertoggle'
+
   use 'ludovicchabant/vim-gutentags' -- Automatic tags management
   -- UI to select things (files, grep results, open buffers...)
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use {'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
@@ -32,9 +40,22 @@ require('packer').startup(function()
   use 'nvim-treesitter/nvim-treesitter'
   -- Additional textobjects for treesitter
   use 'nvim-treesitter/nvim-treesitter-textobjects'
+
   use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
-end)
+end) 
+
+--Set highlight on search
+vim.o.hlsearch = false
+
+--Make line numbers default
+vim.wo.number = true
+
+--Enable mouse mode
+vim.o.mouse = 'a'
+--Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme onedark]]
